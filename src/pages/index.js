d668ef6c4d08a81c2006b40fee65bb9e4967d4b6
aersosi <aersosi@avodaq.com>
2022-03-22 13:08:15 +0100

    [improve] - nav

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,8 +6,8 @@ import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import styles from './index.module.scss';
 import HomepageFeatures from '@site/src/components/HomepageFeatures';
 
-import ReLogo from '@site/static/img/re_logo.svg';
 import {Fade} from "react-awesome-reveal";
+import ReLogo from "@avo/monorepo/doku_libs/static/img/rescoped_logo_white.svg";
 import {fadeProps} from "@avo/monorepo/doku_libs/reveal_animations/fadeAnimation";
 
 
@@ -20,7 +20,7 @@ function HomepageHeader() {
             <div className="container">
 
                 <Fade {...fadeProps}>
-                    <ReLogo title="Docusaurus Logo" className="logo"/>
+                    <ReLogo title="Docusaurus Logo" className="logo" style={{width: 120}}/>
                     <h1 className="hero__title">{siteConfig.title}</h1>
                     <p className="hero__subtitle">{siteConfig.tagline}</p>
                     <div className={styles.buttons}>
