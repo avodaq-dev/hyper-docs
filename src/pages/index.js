0c51f199cf04cb4d0fdaad64f41a6cc99151a817
aersosi <aersosi@avodaq.com>
2022-03-25 09:12:07 +0100

    [chore] - merge main -> dev

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,10 +6,9 @@ import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import styles from './index.module.scss';
 import HomepageFeatures from '@site/src/components/HomepageFeatures';
 
-import ReLogo from '@site/static/img/re_logo.svg';
 import {Fade} from "react-awesome-reveal";
+// import ReLogo from "@avo/monorepo/doku_libs/static/img/rescoped_logo_white.svg";
 import {fadeProps} from "@avo/monorepo/doku_libs/reveal_animations/fadeAnimation";
-
 
 
 function HomepageHeader() {
@@ -20,7 +19,7 @@ function HomepageHeader() {
             <div className="container">
 
                 <Fade {...fadeProps}>
-                    <ReLogo title="Docusaurus Logo" className="logo"/>
+                    {/*<ReLogo title="Docusaurus Logo" className="logo" style={{width: 120}}/>*/}
                     <h1 className="hero__title">{siteConfig.title}</h1>
                     <p className="hero__subtitle">{siteConfig.tagline}</p>
                     <div className={styles.buttons}>
@@ -42,7 +41,7 @@ export default function Home() {
     const {siteConfig} = useDocusaurusContext();
     return (
         <Layout
-            title={`Hello from ${siteConfig.title}`}
+            title={`${siteConfig.title}`}
             description="Description will go into a meta tag in <head />">
             <HomepageHeader/>
             <main>
